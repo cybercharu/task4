52867169d384aabc4863f0a0e85281b7e067c94f
cybercharu <sharmacharu379@gmail.com>
2025-08-03 12:14:30 +0530

    first commit

---
+++ app/database.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+
+load_dotenv()  
+
+MONGO_URI = os.getenv("MONGO_URI")
+client =  MongoClient(MONGO_URI)
+db = client["customer_data"] 
+collection = db["users"]
+movie_collection = db["movies"]
